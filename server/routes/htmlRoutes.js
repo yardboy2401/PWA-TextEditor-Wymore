c0ec80c82e547409489e28bdbc92da8297b0e6be
Jeff Wymore <yardboy2401@gmail.com>
2022-02-05 17:46:11 -0800

    htmlRoutes.js cleaned up and comments added

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,7 @@
-const path = require('path');
+//require path module
+const path
 
+//export app route
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
