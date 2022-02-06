7371f6fcf94e96394ab844d2c221722713c5c2fa
Jeff Wymore <yardboy2401@gmail.com>
2022-02-05 17:47:50 -0800

    server.js file cleaned up and comments added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
+//require for express
 const express = require('express');
 
+//creating app variable 
 const app = express();
+
+//declaring PORT numbers
 const PORT = process.env.PORT || 3000;
 
+//app.use for static/urlencoded/JSON data types
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//require htmlRoutes
 require('./routes/htmlRoutes')(app);
 
+//app.listen for the PORT
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
